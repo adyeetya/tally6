98a1dcbe170a5be416683897d3a3324f008bda7c
adyeetya <2612adityasingh2000@gmail.com>
2023-12-13 21:54:18 +0530

    without .next

---
+++ components/Index/Hero/carousel.tsx
@@ -0,0 +1,330 @@
+import Carousel from 'react-bootstrap/Carousel'
+import styles from './hero.module.css'
+function UncontrolledExample() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <div
+          className={styles['container']}
+        
+        >
+          <div className={styles['left-content']}>
+            <h1>Authorized ERP Solutions</h1>
+            <div
+              className="animated3 mt-5"
+              style={{ display: 'flex', marginTop: '20px' }}
+            >
+              <a
+                className="btn btn-2"
+                href="Custom-Built-Solutions-In-Tally-ERP9.php"
+                style={{
+                  height: '80px',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  display: 'flex',
+                  width: '190px',
+                }}
+              >
+                {' '}
+                <span className="btn-icon btn-arrow" />
+                <span className="btn-text my-auto">Learn More</span>
+              </a>
+              <a
+                className="btn btn-2 "
+                href="Contact-Authorized-Tally-Solution-Unit.php"
+                style={{
+                  height: '80px',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  display: 'flex',
+                  width: '190px',
+                }}
+              >
+                {' '}
+                <span className="btn-icon btn-arrow" />
+                <span className="btn-text ">Contact Us</span>
+              </a>
+            </div>
+          </div>
+          <div className={styles['right-content']}>
+            <div
+              style={{
+                width: '100%', // Take full width
+                maxWidth: '400px', // Max width for large screens
+                height: 'auto', // Maintain aspect ratio
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <img
+                className={styles['image']}
+                src="/images/Asset-3.png"
+                alt="car-img"
+                style={{ width: '393px', height: '236px' }}
+              />
+            </div>
+          </div>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div
+          className={styles['container']}
+         
+        >
+          <div className={styles['left-content']}>
+            <h1>Authorized ERP Solutions</h1>
+            <div
+              className="animated3 mt-5"
+              style={{ display: 'flex', marginTop: '20px' }}
+            >
+              <a
+                className="btn btn-2"
+                href="Custom-Built-Solutions-In-Tally-ERP9.php"
+                style={{
+                  height: '80px',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  display: 'flex',
+                  width: '190px',
+                }}
+              >
+                {' '}
+                <span className="btn-icon btn-arrow" />
+                <span className="btn-text my-auto">Learn More</span>
+              </a>
+              <a
+                className="btn btn-2 "
+                href="Contact-Authorized-Tally-Solution-Unit.php"
+                style={{
+                  height: '80px',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  display: 'flex',
+                  width: '190px',
+                }}
+              >
+                {' '}
+                <span className="btn-icon btn-arrow" />
+                <span className="btn-text ">Contact Us</span>
+              </a>
+            </div>
+          </div>
+          <div className={styles['right-content']}>
+            <div
+              style={{
+                width: '100%', // Take full width
+                maxWidth: '400px', // Max width for large screens
+                height: 'auto', // Maintain aspect ratio
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <img
+                className={styles['image']}
+                src="/images/Asset-4.png"
+                alt="car-img"
+                style={{ width: '393px', height: '236px' }}
+              />
+            </div>
+          </div>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div
+          className={styles['container']}
+         
+        >
+          <div className={styles['left-content']}>
+            <h1>Authorized ERP Solutions</h1>
+            <div
+              className="animated3 mt-5"
+              style={{ display: 'flex', marginTop: '20px' }}
+            >
+              <a
+                className="btn btn-2"
+                href="Custom-Built-Solutions-In-Tally-ERP9.php"
+                style={{
+                  height: '80px',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  display: 'flex',
+                  width: '190px',
+                }}
+              >
+                {' '}
+                <span className="btn-icon btn-arrow" />
+                <span className="btn-text my-auto">Learn More</span>
+              </a>
+              <a
+                className="btn btn-2 "
+                href="Contact-Authorized-Tally-Solution-Unit.php"
+                style={{
+                  height: '80px',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  display: 'flex',
+                  width: '190px',
+                }}
+              >
+                {' '}
+                <span className="btn-icon btn-arrow" />
+                <span className="btn-text ">Contact Us</span>
+              </a>
+            </div>
+          </div>
+          <div className={styles['right-content']}>
+            <div
+              style={{
+                width: '100%', // Take full width
+                maxWidth: '400px', // Max width for large screens
+                height: 'auto', // Maintain aspect ratio
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <img
+                className={styles['image']}
+                src="/images/Asset-5.png"
+                alt="car-img"
+                style={{ width: '393px', height: '236px' }}
+              />
+            </div>
+          </div>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div
+          className={styles['container']}
+         
+        >
+          <div className={styles['left-content']}>
+            <h1>Authorized ERP Solutions</h1>
+            <div
+              className="animated3 mt-5"
+              style={{ display: 'flex', marginTop: '20px' }}
+            >
+              <a
+                className="btn btn-2"
+                href="Custom-Built-Solutions-In-Tally-ERP9.php"
+                style={{
+                  height: '80px',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  display: 'flex',
+                  width: '190px',
+                }}
+              >
+                {' '}
+                <span className="btn-icon btn-arrow" />
+                <span className="btn-text my-auto">Learn More</span>
+              </a>
+              <a
+                className="btn btn-2 "
+                href="Contact-Authorized-Tally-Solution-Unit.php"
+                style={{
+                  height: '80px',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  display: 'flex',
+                  width: '190px',
+                }}
+              >
+                {' '}
+                <span className="btn-icon btn-arrow" />
+                <span className="btn-text ">Contact Us</span>
+              </a>
+            </div>
+          </div>
+          <div className={styles['right-content']}>
+            <div
+              style={{
+                width: '100%', // Take full width
+                maxWidth: '400px', // Max width for large screens
+                height: 'auto', // Maintain aspect ratio
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <img
+                className={styles['image']}
+                src="/images/Asset-6.png"
+                alt="car-img"
+                style={{ width: '393px', height: '236px' }}
+              />
+            </div>
+          </div>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div
+          className={styles['container']}
+        
+        >
+          <div className={styles['left-content']}>
+            <h1>Authorized ERP Solutions</h1>
+            <div
+              className="animated3 mt-5"
+              style={{ display: 'flex', marginTop: '20px' }}
+            >
+              <a
+                className="btn btn-2"
+                href="Custom-Built-Solutions-In-Tally-ERP9.php"
+                style={{
+                  height: '80px',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  display: 'flex',
+                  width: '190px',
+                }}
+              >
+                {' '}
+                <span className="btn-icon btn-arrow" />
+                <span className="btn-text my-auto">Learn More</span>
+              </a>
+              <a
+                className="btn btn-2 "
+                href="Contact-Authorized-Tally-Solution-Unit.php"
+                style={{
+                  height: '80px',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  display: 'flex',
+                  width: '190px',
+                }}
+              >
+                {' '}
+                <span className="btn-icon btn-arrow" />
+                <span className="btn-text ">Contact Us</span>
+              </a>
+            </div>
+          </div>
+          <div className={styles['right-content']}>
+            <div
+              style={{
+                width: '100%', // Take full width
+                maxWidth: '400px', // Max width for large screens
+                height: 'auto', // Maintain aspect ratio
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <img
+                className={styles['image']}
+                src="/images/Asset-7.png"
+                alt="car-img"
+                style={{ width: '393px', height: '236px' }}
+              />
+            </div>
+          </div>
+        </div>
+      </Carousel.Item>
+    </Carousel>
+  )
+}
+
+export default UncontrolledExample
